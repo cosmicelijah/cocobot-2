18d91ad395029f47e3dc787bd8668b3b9c51a46e
watsonet <watsonet@rose-hulman.edu>
2023-05-02 00:50:14 -0400

    Add image thing

--- index.js
+++ index.js
@@ -3,31 +3,78 @@ const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-for (const f of commandFiles) {
-  const fPath = path.join(commandsPath, f);
-  const command = require(fPath);
+/**
+ * Command Handler
+ */
 
-  if ('data' in command && 'execute' in command) {
-    client.commands.set(command.data.name, command);
-  } else {
-    console.log(`Warning: file ${fPath} does not contain a 'data' or 'execute' property`);
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`Warning: file ${filePath} does not contain a 'data' or 'execute' property`);
+    }
   }
 }
 
-client.once(Events.ClientReady, c => {
+/**
+ * Log In Success
+ */
+
+client.once(Events.ClientReady, (c) => {
   console.log(`Ready\nLogged in as ${c.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, interaction => {
+/**
+ * Interaction Handler
+ */
+
+client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  console.log(interation);
+  // console.log(interaction);
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`Error: no command matching ${interaction.commandName} was found`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+
+  } catch (error) {
+    console.error(error);
+
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: 'There was an error executing this command',
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: 'There was an error executing this command',
+        ephemeral: true,
+      });
+    }
+  }
 })
+
+/**
+ * Log In
+ */
 
 client.login(token);
